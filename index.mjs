d9b55405061dd4277e397c6fa9e01424e0a1ab5d
Simon Asika <asika32764@gmail.com>
2025-03-03 01:00:21 +0800

    Fix exports issue

---
+++ index.mjs
@@ -0,0 +1,25 @@
+import chalk from 'chalk';
+
+export default function showEnv(exposes = {}) {
+  return {
+    name: 'show-env',
+
+    configResolved(config) {
+      if (exposes && Array.isArray(exposes) && exposes.length > 0) {
+        console.log();
+        console.log(chalk.yellow('MODE:'), chalk.green(config.mode));
+
+        console.log(chalk.yellow('ENVIRONMENT VARIABLES'));
+
+        for (const env of exposes) {
+          console.log(
+            `  ${env}:`,
+            chalk.green(config.env[env] ?? import.meta.env?.[env])
+          );
+        }
+
+        console.log();
+      }
+    }
+  }
+}
